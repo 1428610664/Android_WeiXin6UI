6d314d17622fa10da0113d121a830ca3dc49b688
SpiritWolf2015 <522414019@qq.com>
2015-05-24 23:09:18 +0800

    1.0
    慕课网，微信6.0UI效果

---
+++ src/com/gzc/weixin6ui/MainActivity.java
@@ -0,0 +1,27 @@
+package com.gzc.weixin6ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+/**
+ * 
+ * @author gzc
+ *
+ */
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);	
+		this.setContentView(R.layout.activity_main);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		this.getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	
+}
