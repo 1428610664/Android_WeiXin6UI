84309f3a54bba15f0065c9b68508f601479a8a7c
SpiritWolf2015 <522414019@qq.com>
2015-06-07 22:57:44 +0800

    1.3
    底部自定义VIEW，绘制图标变色，绘制文字。变的颜色还有问题，不是绿色。标题栏的加号还没搞出来，出来的菜单没图标。

--- src/com/gzc/weixin6ui/ChangeColorIconWithText.java
+++ src/com/gzc/weixin6ui/ChangeColorIconWithText.java
@@ -3,11 +3,15 @@ package com.gzc.weixin6ui;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
@@ -36,7 +40,7 @@ public class ChangeColorIconWithText extends View {
 	private Bitmap mBitmap;
 	private Paint mPaint;
 
-	private float mAlpha;
+	private float mAlpha = 1F;
 
 	private Rect mIconRect;
 	private Rect mTextBound;
@@ -100,7 +104,7 @@ public class ChangeColorIconWithText extends View {
 	}
 	// ==============构造函数==============
 	
-	
+	// 检测View组件及其子组件的大小
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -115,11 +119,61 @@ public class ChangeColorIconWithText extends View {
 		mIconRect = new Rect(left, top, left + iconWidth, top + iconWidth);
 	}
 	
+	// 当组件将要绘制它的内容时，可以理解成U3D的OnGUI
 	@Override
 	protected void onDraw(Canvas canvas) {		
 		canvas.drawBitmap(mIconBitmap, null, this.mIconRect, null);
+		
+		int alpha = (int)Math.ceil(255 * this.mAlpha);
+		
+		// 绘制图标
+		// 内存中去准备mBitmap，setAlpha，纯色，xfermode，图标。
+		setupTargetBitmap(alpha);
+		
+		// 绘制文字
+		// 1、绘制原文本。2、绘制变色文本。
+		drawSourceText(canvas, alpha);
+		drawTargetText(canvas, alpha);
+		
+		canvas.drawBitmap(mBitmap, 0, 0, null);
 	}
 	
-	
+	/** 在内存中绘制可变色的icon */
+	private void setupTargetBitmap(int alpha){
+		this.mBitmap = Bitmap.createBitmap(this.getMeasuredWidth(), this.getMeasuredHeight(),Config.ARGB_8888);
+		this.mCanvas = new Canvas(mBitmap);		
+		mPaint = new Paint();
+		mPaint.setColor(mColor);
+		mPaint.setAntiAlias(true);
+		mPaint.setDither(true);
+		mPaint.setAlpha(alpha);
+		
+		this.mCanvas.drawRect(mIconRect, mPaint);
+		
+		this.mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));		
+		this.mPaint.setAlpha(255);
+		
+		this.mCanvas.drawBitmap(mIconBitmap, null, this.mIconRect,this.mPaint);
+	}
 
+	/** 绘制原文本  */
+	private void drawSourceText(Canvas canvas, int alpha){
+		mTextPaint.setColor(0xff333333);
+		mTextPaint.setAlpha(255 - alpha);
+		int x = getMeasuredWidth() / 2 - mTextBound.width() / 2;
+		int y = mIconRect.bottom + mTextBound.height();
+		canvas.drawText(mText, x, y, mTextPaint);
+	}
+	
+	/** 绘制变色文本  */
+	private void drawTargetText(Canvas canvas, int alpha){
+		mTextPaint.setColor(mColor);
+		Log.i("绘制变色文本", "color = " + mColor);		
+		
+		mTextPaint.setAlpha(alpha);
+		int x = getMeasuredWidth() / 2 - mTextBound.width() / 2;
+		int y = mIconRect.bottom + mTextBound.height();
+		canvas.drawText(mText, x, y, mTextPaint);
+	}
+	
 }
